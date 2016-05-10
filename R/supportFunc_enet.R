c3b6535849cd3fc2a75ec4daac251ea4f62d2592
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2016-05-10 16:58:43 -0700

    added to code to separate files

---
+++ R/supportFunc_enet.R
@@ -0,0 +1,92 @@
+library(glmnet)
+
+## Elastic net
+enet = function(X, Y, alpha, lambda=NULL){
+  library(glmnet)
+  # Run Elastic net classifier
+  fit <- glmnet(X, Y, family="binomial", alpha = alpha)
+  cv.fit <- cv.glmnet(X, Y, family="binomial")
+  if(is.null(lambda)){
+    set.seed(1)
+    lambda = cv.fit$lambda.min
+  } else {
+    lambda = lambda
+  }
+  Coefficients <- coef(fit, s = lambda)
+  Active.Index <- which(Coefficients != 0)
+  Active.Coefficients  <- Coefficients[Active.Index,]
+  enet.panel <- names(Active.Coefficients)[-1]
+  enet.panel.length <- length(enet.panel)
+  return(list(X = X, Y = Y, fit = fit, enet.panel = enet.panel, lambda = lambda, alpha = alpha))
+}
+
+## cross-validation Enet
+perf.enet = function (object, validation = c("Mfold", "loo"), folds = 10,
+  progressBar = TRUE, near.zero.var = FALSE)
+{
+  X = object$X
+  Y = object$Y
+  n = nrow(X)
+  alpha = object$alpha
+  nzv = nearZeroVar(X)
+  if (length(nzv$Position > 0)) {
+    warning("Zero- or near-zero variance predictors.\nReset predictors matrix to not near-zero variance predictors.\nSee $nzv for problematic predictors.")
+    X = X[, -nzv$Position, drop = TRUE]
+    if (ncol(X) == 0) {
+      stop("No more predictors after Near Zero Var has been applied!")
+    }
+  }
+
+  if (validation == "Mfold") {
+    if (is.list(folds)) {
+      if (length(folds) < 2 | length(folds) > n)
+        stop("Invalid number of folds.")
+      if (length(unique(unlist(folds))) != n)
+        stop("Invalid folds.")
+      M = length(folds)
+    }
+    else {
+      if (is.null(folds) || !is.numeric(folds) || folds <
+          2 || folds > n)
+        stop("Invalid number of folds.")
+      else {
+        M = round(folds)
+        folds = split(sample(1:n), rep(1:M, length = n))
+      }
+    }
+  }
+  else {
+    folds = split(1:n, rep(1:n, length = n))
+    M = n
+  }
+
+  probs <- list()
+  stop.user = FALSE
+  if (progressBar == TRUE)
+    pb <- txtProgressBar(style = 3)
+  for (i in 1:M) {
+    if (progressBar == TRUE)
+      setTxtProgressBar(pb, i/M)
+    omit = folds[[i]]
+    if (length(omit) == 1)
+      stop.user = TRUE
+    X.train = X[-omit, ]
+    Y.train = Y[-omit]
+    X.test = matrix(X[omit, ], nrow = length(omit))
+    enet.res = suppressWarnings(enet(X.train, Y.train, alpha = alpha, lambda = NULL))
+    probs[[i]] <- predict(enet.res$fit, newx=X.test, s =  lambda, type='response')
+  }
+  probs <- unlist(probs)
+  if (stop.user == TRUE & validation == "Mfold")
+    stop("The folds value was set too high to perform cross validation. Choose validation = \"loo\" or set folds to a lower value")
+  if (progressBar == TRUE)
+    cat("\n")
+
+  result = list()
+  result$nzvX = nzv$Position
+  result$probs = probs
+  method = "enet.mthd"
+  result$meth = "enet.mthd"
+  class(result) = c("perf", method)
+  return(invisible(result))
+}
--- R/supportFunc_splsda.R
+++ R/supportFunc_splsda.R
@@ -1,6 +1,4 @@
 library(mixOmics)
-library(pROC)
-library(OptimalCutpoints)
 ## Predict new data with splsda model
 perf.splsda2 = function(object, method.predict = c("all", "max.dist", "centroids.dist",
   "mahalanobis.dist"), validation = c("Mfold", "loo"), folds = 10,
@@ -119,17 +117,3 @@ perf.splsda2 = function(object, method.predict = c("all", "max.dist", "centroids
   return(invisible(result))
 }
 
-## determine AUC from predictions and true labels
-tperformance = function(weights, trueLabels, direction){
-  ## Determine optimal cut-off values and associated performance measures
-  df = data.frame(prob = weights,
-    status = model.matrix(~factor(as.character(trueLabels), levels = levels(trueLabels)))[, 2])
-  roc.score = roc(response = as.character(trueLabels), predictor = weights, plot = TRUE, percent = TRUE, na.rm =TRUE, direction = direction)
-
-  optimal.cutpoint.Youden <- optimal.cutpoints(X = "prob", status = "status", tag.healthy = 0, methods = "Youden",
-    data = df, control = control.cutpoints(), ci.fit = FALSE,
-    conf.level = 0.95, trace = FALSE, pop.prev = 0.5)
-  optimalValues <- round(c(summary(optimal.cutpoint.Youden)$p.table$Global$Youden[[1]][1:5, ], roc.score$auc), 2)
-  names(optimalValues) <- c(names(optimalValues)[-length(names(optimalValues))], "AUC")
-  optimalValues
-}
