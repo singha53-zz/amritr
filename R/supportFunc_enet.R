df45c0449d0df42664cb757f7cf0f59e9001fe30
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2016-09-22 17:20:39 -0700

    probs output from enet()

--- R/supportFunc_enet.R
+++ R/supportFunc_enet.R
@@ -60,10 +60,10 @@ enet = function(X, Y, alpha, lambda=NULL, family, X.test=NULL, Y.test=NULL){
       names(perfTest) <- c(names(classError), "ER", "BER")
     }
   } else {
-    perfTest <- predictResponse <- NA
+    perfTest <- predictResponse <- probs <- NA
   }
 
-  return(list(X = X, Y = Y, fit = fit, enet.panel = enet.panel, lambda = lambda, alpha = alpha, family = family, perfTest=perfTest, predictResponse=predictResponse))
+  return(list(X = X, Y = Y, fit = fit, enet.panel = enet.panel, lambda = lambda, alpha = alpha, family = family, probs = probs, perfTest=perfTest, predictResponse=predictResponse))
 }
 
 
