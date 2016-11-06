ae9b51614afd3e3e4803d0410ba2b794563a1bf8
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2016-11-05 21:55:22 -0700

    fix enet() and rforest()

--- R/supportFunc_enet.R
+++ R/supportFunc_enet.R
@@ -10,26 +10,25 @@
 #' @param filter = "none" or "p.value"
 #' @param topranked = 50 (top number of features to select and build a classifier)
 #' @export
-## Elastic net
 enet = function (X, Y, alpha, lambda = NULL, family, X.test = NULL,
   Y.test = NULL, filter = "p.value", topranked = 50)
 {
-  library(limma)
-  library(glmnet)
+  library(glmnet); library(limma); library(pROC); library(OptimalCutpoints);
+
   if (filter == "none") {
-    X <- X
+    X1 <- X
   }
   if (filter == "p.value") {
     design <- model.matrix(~Y)
     fit <- eBayes(lmFit(t(X), design))
     top <- topTable(fit, coef = 2, adjust.method = "BH",
       n = nrow(fit))
-    X <- X[, rownames(top)[1:topranked]]
+    X1 <- X[, rownames(top)[1:topranked]]
   }
 
   if (family == "binomial") {
-    fit <- glmnet(X, Y, family = "binomial", alpha = alpha)
-    cv.fit <- cv.glmnet(X, Y, family = "binomial")
+    fit <- glmnet(X1, Y, family = "binomial", alpha = alpha)
+    cv.fit <- cv.glmnet(X1, Y, family = "binomial")
     if (is.null(lambda)) {
       lambda = cv.fit$lambda.min
     }
@@ -43,9 +42,9 @@ enet = function (X, Y, alpha, lambda = NULL, family, X.test = NULL,
     enet.panel.length <- length(enet.panel)
   }
   if (family == "multinomial") {
-    fit <- glmnet(X, Y, family = "multinomial", alpha = alpha,
+    fit <- glmnet(X1, Y, family = "multinomial", alpha = alpha,
       type.multinomial = "grouped")
-    cv.fit <- cv.glmnet(X, Y, family = "multinomial")
+    cv.fit <- cv.glmnet(X1, Y, family = "multinomial")
     if (is.null(lambda)) {
       lambda = cv.fit$lambda.min
     }
@@ -90,6 +89,99 @@ enet = function (X, Y, alpha, lambda = NULL, family, X.test = NULL,
     predictResponse = predictResponse, filter = filter, topranked = topranked))
 }
 
+#' interal function (enet cross-validation)
+#'
+#' Estimate test error of elastic net panel
+#' @param X nxp matrix - training dataset
+#' @param Y categorical variables
+#' @param M = # of folds
+#' @param folds list of length M specifiying which samples are in which fold
+#' @param family "binomial" or "multinomial"
+#' @param filter = "none" or "p.value"
+#' @param topranked = 50 (top number of features to select and build a classifier)
+#' @export
+enetCV = function (X, Y, alpha, lambda, M, folds, progressBar, family,
+  filter, topranked)
+{
+  library(pROC); library(limma); library(glmnet); library(OptimalCutpoints)
+
+  probs <- predictResponseList <- enet.panel <- list()
+  if (progressBar == TRUE)
+    pb <- txtProgressBar(style = 3)
+  for (i in 1:M) {
+    if (progressBar == TRUE)
+      setTxtProgressBar(pb, i/M)
+    omit = folds[[i]]
+    X.train = X[-omit, ]
+    Y.train = Y[-omit]
+
+    if (filter == "none") {
+      X.train1 <- X.train
+    }
+    if (filter == "p.value") {
+      design <- model.matrix(~Y.train)
+      fit <- eBayes(lmFit(t(X.train), design))
+      top <- topTable(fit, coef = 2, adjust.method = "BH", n = nrow(fit))
+      X.train1 <- X.train[, rownames(top)[1:topranked]]
+    }
+    X.test1 = X[omit, colnames(X.train1), drop = FALSE]
+
+    if (family == "binomial") {
+      fit <- glmnet(X.train1, Y.train, family = "binomial", alpha = alpha)
+      cv.fit <- cv.glmnet(X.train1, Y.train, family = "binomial")
+      if (is.null(lambda)) {
+        lambda = cv.fit$lambda.min
+      }
+      else {
+        lambda = lambda
+      }
+      Coefficients <- coef(fit, s = lambda)
+      Active.Index <- which(Coefficients[, 1] != 0)
+      Active.Coefficients <- Coefficients[Active.Index, ]
+      enet.panel[[i]] <- names(Active.Coefficients)[-1]
+    }
+    if (family == "multinomial") {
+      fit <- glmnet(X.train1, Y.train, family = "multinomial", alpha = alpha,
+        type.multinomial = "grouped")
+      cv.fit <- cv.glmnet(X.train1, Y.train, family = "multinomial")
+      if (is.null(lambda)) {
+        lambda = cv.fit$lambda.min
+      }
+      else {
+        lambda = lambda
+      }
+      Coefficients <- coef(fit, s = lambda)
+      Active.Index <- which(Coefficients[[1]][, 1] != 0)
+      Active.Coefficients <- Coefficients[[1]][Active.Index,
+        ]
+      enet.panel[[i]] <- names(Active.Coefficients)[-1]
+    }
+
+    probs[[i]] <- predict(fit, newx = X.test1, s = lambda, type = "response")
+    predictResponseList[[i]] <- predict(fit, newx = X.test1, s = lambda, type = "class")
+  }
+  predictResponse <- unlist(predictResponseList)
+  if (family == "binomial") {
+    probs <- unlist(probs)
+    trueLabels = Y[unlist(folds)]
+    library(pROC)
+    library(OptimalCutpoints)
+    perf <- amritr::tperformance(weights = probs, trueLabels = trueLabels)
+  }
+  else {
+    trueLabels = Y[unlist(folds)]
+    mat <- table(trueLabels, predictResponse)
+    mat2 <- mat
+    diag(mat2) <- 0
+    classError <- colSums(mat2)/colSums(mat)
+    er <- sum(mat2)/sum(mat)
+    ber <- mean(classError)
+    perf <- c(classError, er, ber)
+    names(perf) <- c(names(classError), "ER", "BER")
+  }
+  return(list(probs = probs, trueLabels = trueLabels, perf = perf,
+    enet.panel = enet.panel, predictResponse = predictResponse))
+}
 
 #' cross-validation function for elastic net panel
 #'
@@ -100,10 +192,9 @@ enet = function (X, Y, alpha, lambda = NULL, family, X.test = NULL,
 #' @param family "binomial" or "multinomial"
 #' @param lambda = strength of elastic net penalty
 #' @param M = # of folds
-#' @param folds list of length iter with each element a list of length M
-#' @param X.test nxp matrx  - test dataset
-#' @param filter = "none" or "p.value"
-#' @param topranked = 50 (top number of features to select and build a classifier)
+#' @param iter = number of times to repeat cross-valiation
+#' @param threads - number of cpus (each cross-valiation scheme performed on a separate node)
+#' @param progressBar - show progressbar (TRUE/FALE)
 #' @export
 perf.enet = function (object, validation = c("Mfold", "loo"), M = 5, iter = 10,
   threads = 4, progressBar = TRUE)
@@ -151,75 +242,10 @@ perf.enet = function (object, validation = c("Mfold", "loo"), M = 5, iter = 10,
   result$folds = folds
   result$probs = cv$probs
   result$trueLabels = cv$trueLabels
+  result$panels = cv$enet.panel
   result$perf = perf
   method = "enet.mthd"
   result$meth = "enet.mthd"
   class(result) = c("perf", method)
   return(invisible(result))
-}
-
-#' interal function (enet cross-validation)
-#'
-#' build an elastic net classification panel
-#' @param object - elastic net classifier
-#' @param validation - Mfold or LOOCV
-#' @param M - # of folds
-#' @param iter - number of times to repeat cross-validation
-#' @param threads - number of cpus (each cross-valiation scheme performed on a separate node)
-#' @param progressBar - show progressbar (TRUE/FALE)
-#' @export
-enetCV = function (X, Y, alpha, lambda, M, folds, progressBar, family,
-  filter, topranked)
-{
-  library(limma)
-  library(glmnet)
-  if (filter == "none") {
-    X <- X
-  }
-  if (filter == "p.value") {
-    design <- model.matrix(~Y)
-    fit <- eBayes(lmFit(t(X), design))
-    top <- topTable(fit, coef = 2, adjust.method = "BH",
-      n = nrow(fit))
-    X <- X[, rownames(top)[1:topranked]]
-  }
-
-  probs <- predictResponseList <- list()
-  if (progressBar == TRUE)
-    pb <- txtProgressBar(style = 3)
-  for (i in 1:M) {
-    if (progressBar == TRUE)
-      setTxtProgressBar(pb, i/M)
-    omit = folds[[i]]
-    X.train = X[-omit, ]
-    Y.train = Y[-omit]
-    X.test = matrix(X[omit, ], nrow = length(omit))
-    enet.res = suppressWarnings(enet(X.train, Y.train, alpha = alpha,
-      lambda = lambda, family = family, filter = filter, topranked = topranked))
-    probs[[i]] <- predict(enet.res$fit, newx = X.test, s = lambda,
-      type = "response")
-    predictResponseList[[i]] <- predict(enet.res$fit, newx = X.test,
-      s = lambda, type = "class")
-  }
-  predictResponse <- unlist(predictResponseList)
-  if (family == "binomial") {
-    probs <- unlist(probs)
-    trueLabels = Y[unlist(folds)]
-    library(pROC)
-    library(OptimalCutpoints)
-    perf <- amritr::tperformance(weights = probs, trueLabels = trueLabels)
-  }
-  else {
-    trueLabels = Y[unlist(folds)]
-    mat <- table(trueLabels, predictResponse)
-    mat2 <- mat
-    diag(mat2) <- 0
-    classError <- colSums(mat2)/colSums(mat)
-    er <- sum(mat2)/sum(mat)
-    ber <- mean(classError)
-    perf <- c(classError, er, ber)
-    names(perf) <- c(names(classError), "ER", "BER")
-  }
-  return(list(probs = probs, trueLabels = trueLabels, perf = perf,
-    predictResponse = predictResponse))
 }
--- R/supportFunc_rf.R
+++ R/supportFunc_rf.R
@@ -1,123 +1,182 @@
-#' table of classification performances
+#' Random forest classification panel
 #'
-#' takes in predited weights and true labels and determines performance characterisitcs
-#' @param weights are the predicted scores/probablities of test data
-#' @param trubeLabels are the true labels associated with the test data
-#' @param direction = "auto", ">", "<"
+#' build random forest classification panel
+#' @param X nxp matrix - training dataset
+#' @param Y categorical variables
+#' @param family "binomial" or "multinomial"
+#' @param X.test nxp matrx  - test dataset
+#' @param filter = "none" or "p.value"
+#' @param topranked = 50 (top number of features to select and build a classifier)
 #' @export
-rforest = function(X, Y, X.test=NULL, Y.test=NULL){
-  library(randomForest)
-
-  # Run random forest classifier
-  colnames(X) <- paste("Feature", 1:ncol(X), sep=".")
-  y.train0 <- Y[rownames(X)]
-  fit = randomForest(y.train0 ~ ., data=X, importance=TRUE, proximity=TRUE)
-
-
-  if(!is.null(X.test)){
-    colnames(X.test) <- paste("Feature", 1:ncol(X), sep=".")
-    ## assess panel performance using test data if available
-    predictResponse <- predict(fit, as.matrix(X.test), type='response')
-
-    mat <- table(Y.test, predictResponse)
-    mat2 <- mat
-    diag(mat2) <- 0
-    classError <- colSums(mat2)/colSums(mat)
-    er <- sum(mat2)/sum(mat)
-    ber <- mean(classError)
-    perfTest <- c(classError, er, ber)
-    names(perfTest) <- c(names(classError), "ER", "BER")
-  } else {
-    perfTest <- predictResponse <- NA
+rforest = function (X, Y, X.test = NULL, Y.test = NULL, family = "binomial", filter = "p.value", topranked = 50)
+{
+  library(randomForest); library(limma); library(pROC); library(OptimalCutpoints);
+  X1 <- X
+  featIndex <- colnames(X1)
+  names(featIndex) <- paste("f", 1:ncol(X1), sep = "_")
+  colnames(X1) <- names(featIndex)
+  if (filter == "none") {
+    X1 <- X1
+  }
+  if (filter == "p.value") {
+    design <- model.matrix(~Y)
+    fit <- eBayes(lmFit(t(X1), design))
+    top <- topTable(fit, coef = 2, adjust.method = "BH",
+      n = nrow(fit))
+    X1 <- X1[, rownames(top)[1:topranked]]
   }
 
-  return(list(X = X, Y = Y, fit = fit, perfTest=perfTest, predictResponse=predictResponse))
+  rf.panel <- as.character(featIndex[colnames(X1)])
+  y.train0 <- Y[rownames(X1)]
+  fit = randomForest(y.train0 ~ ., data = X1, importance = TRUE,
+    proximity = TRUE)
+  if (!is.null(X.test)) {
+    colnames(X.test) <- paste("Feature", 1:ncol(X1), sep = ".")
+    predictResponse <- predict(fit, as.matrix(X.test), type = "response")
+    probs <- predict(fit, as.matrix(X.test), type = "vote")[, levels(Y)[2]]
+    if(family == "binomial"){
+      perfTest <- amritr::tperformance(weights = as.numeric(as.matrix(probs)),
+        trueLabels = Y.test)
+    }
+    if(family == "multinomial"){
+      mat <- table(Y.test, predictResponse)
+      mat2 <- mat
+      diag(mat2) <- 0
+      classError <- colSums(mat2)/colSums(mat)
+      er <- sum(mat2)/sum(mat)
+      ber <- mean(classError)
+      perfTest <- c(classError, er, ber)
+      names(perfTest) <- c(names(classError), "ER", "BER")
+    }
+  }
+  else {
+    perfTest <- predictResponse <- NA
+  }
+  return(list(X = X, Y = Y, fit = fit, perfTest = perfTest, rf.panel = rf.panel,
+    predictResponse = predictResponse, family = family, filter = filter, topranked = topranked))
 }
 
 
-#' table of classification performances
+#' cross-validation for a random forest panel
 #'
-#' takes in predited weights and true labels and determines performance characterisitcs
-#' @param weights are the predicted scores/probablities of test data
-#' @param trubeLabels are the true labels associated with the test data
-#' @param direction = "auto", ">", "<"
+#' Estimate test error of random forest panel
+#' @param X nxp matrix - training dataset
+#' @param Y categorical variables
+#' @param M = # of folds
+#' @param folds list of length M specifiying which samples are in which fold
+#' @param family "binomial" or "multinomial"
+#' @param filter = "none" or "p.value"
+#' @param topranked = 50 (top number of features to select and build a classifier)
 #' @export
-rfCV = function(X, Y, M, folds, progressBar){
-  probs <- predictResponseList <- list()
+rfCV = function(X, Y, M, folds, progressBar, family, filter, topranked)
+{
+  library(pROC); library(limma); library(randomForest); library(OptimalCutpoints)
+
+  X1 <- X
+  featIndex <- colnames(X1)
+  names(featIndex) <- paste("f", 1:ncol(X1), sep = "_")
+  colnames(X1) <- names(featIndex)
+
+  probs <- predictResponseList <- rf.cvPanels <- list()
   if (progressBar == TRUE)
     pb <- txtProgressBar(style = 3)
   for (i in 1:M) {
     if (progressBar == TRUE)
       setTxtProgressBar(pb, i/M)
     omit = folds[[i]]
-    X.train = X[-omit, ]
+    X.train = X1[-omit, ]
     Y.train = Y[-omit]
-    X.test = matrix(X[omit, ], nrow = length(omit))
-    colnames(X.test) <- paste("Feature", 1:ncol(X.test), sep=".")
-    rf.res = suppressWarnings(rforest(X=X.train, Y=Y.train))
-    probs[[i]] <- predict(rf.res$fit, X.test, type='response')
-    predictResponseList[[i]] <- predict(rf.res$fit, X.test, type='class')
+
+    if (filter == "none") {
+      X.train1 <- X.train
+    }
+    if (filter == "p.value") {
+      design <- model.matrix(~Y.train)
+      fit <- eBayes(lmFit(t(X.train), design))
+      top <- topTable(fit, coef = 2, adjust.method = "BH", n = nrow(fit))
+      X.train1 <- X.train[, rownames(top)[1:topranked]]
+    }
+    X.test1 = X1[omit, colnames(X.train1), drop = FALSE]
+
+    rf.cvPanels[[i]] <- as.character(featIndex[colnames(X.train1)])
+    rf.res = randomForest(Y.train ~ ., data = X.train1, importance = TRUE,  proximity = TRUE)
+    probs[[i]] <- predict(rf.res, X.test1, type = "vote")[, levels(Y)[2]]
+    predictResponseList[[i]] <- predict(rf.res, X.test1, type = "class")
   }
   predictResponse <- unlist(predictResponseList)
-
-  ## Error rate
+  probs <- unlist(probs)
   trueLabels = Y[unlist(folds)]
-  mat <- table(trueLabels, predictResponse)
-  mat2 <- mat
-  diag(mat2) <- 0
-  classError <- colSums(mat2)/colSums(mat)
-  er <- sum(mat2)/sum(mat)
-  ber <- mean(classError)
-  perf <- data.frame(Error = c(classError, er, ber))
-  perf$ErrorRate <- c(names(classError), "ER", "BER")
 
-  perf
+  if(family == "binomial"){
+    perf <- amritr::tperformance(weights = probs, trueLabels = trueLabels)
+  }
+  if(family == "multinomial"){
+    mat <- table(trueLabels, predictResponse)
+    mat2 <- mat
+    diag(mat2) <- 0
+    classError <- colSums(mat2)/colSums(mat)
+    er <- sum(mat2)/sum(mat)
+    ber <- mean(classError)
+    perf <- c(classError, er, ber)
+    names(perf) <- c(names(classError), "ER", "BER")
+  }
+  return(list(probs = probs, trueLabels = trueLabels, perf = perf,
+    predictResponse = predictResponse, rf.cvPanels = rf.cvPanels))
 }
 
-#' table of classification performances
+
+#' Cross-validation repeated iter number of times
 #'
-#' takes in predited weights and true labels and determines performance characterisitcs
-#' @param weights are the predicted scores/probablities of test data
-#' @param trubeLabels are the true labels associated with the test data
-#' @param direction = "auto", ">", "<"
+#' Estimate the mean+/-SD test error
+#'
+#' @param object = rforest object
+#' @param validation = "Mfold" or "loo
+#' @param M = # of folds
+#' @param iter = number of times to repeat cross-validation
+#' @param threads = number of cores to use (each cross-valiation scheme performed on a separate node)
+#' @param show progress bar or not: progressBar (TRUE/FALSE)
 #' @export
 perf.rf = function (object, validation = c("Mfold", "loo"), M = 5, iter = 10,
   threads = 4, progressBar = TRUE)
 {
-
   library(dplyr)
   X = object$X
   Y = object$Y
   n = nrow(X)
+  filter = object$filter
+  topranked = object$topranked
+  family = object$family
+
   if (validation == "Mfold") {
     folds <- lapply(1:iter, function(i) caret::createFolds(Y, k = M))
     require(parallel)
-    cl <- parallel::makeCluster(mc <- getOption("cl.cores", threads))
-    parallel::clusterExport(cl, varlist=c("rfCV", "rforest", "X", "Y", "M", "folds", "progressBar"), envir=environment())
-    cv <- parallel::parLapply(cl, folds, function(foldsi, X, Y, M, progressBar){
-      rfCV(X=X, Y=Y, M=M, folds = foldsi, progressBar=progressBar)
-    }, X, Y, M, progressBar) %>% amritr::zip_nPure()
+    cl <- parallel::makeCluster(mc <- getOption("cl.cores",
+      threads))
+    parallel::clusterExport(cl, varlist = c("rfCV", "rforest",
+      "X", "Y", "M", "folds", "progressBar", "family", "filter", "topranked"), envir = environment())
+    cv <- parallel::parLapply(cl, folds, function(foldsi,
+      X, Y, M, progressBar, family, filter, topranked) {
+      rfCV(X = X, Y = Y, M = M, folds = foldsi, progressBar = progressBar, family = family,
+        filter = filter, topranked = topranked)
+    }, X, Y, M, progressBar, family, filter, topranked) %>% amritr::zip_nPure()
     parallel::stopCluster(cl)
-
-  } else {
+    perf <- do.call(rbind, cv$perf) %>% as.data.frame %>%
+      gather(ErrName, Err) %>% dplyr::group_by(ErrName) %>%
+      dplyr::summarise(Mean = mean(Err), SD = sd(Err))
+  }
+  else {
     folds = split(1:n, rep(1:n, length = n))
     M = n
-    cv <- rfCV(X, Y, M, folds, progressBar)
+    cv <- rfCV(X, Y, M, folds, progressBar, family, filter, topranked)
+    perf <- cv$perf
   }
 
-  ## Summarise performance results
-  perf0 <- do.call(rbind, cv$Error)
-  colnames(perf0) <- cv$ErrorRate[[1]]
-  perf <- perf0 %>% as.data.frame() %>%
-    gather(ErrName, Err) %>% dplyr::group_by(ErrName) %>%
-    dplyr::summarise(Mean = mean(Err), SD = sd(Err))
 
   result = list()
   result$perf = perf
+  result$panels = cv$rf.cvPanels
   method = "rf.mthd"
   result$meth = "rf.mthd"
   class(result) = c("perf", method)
   return(invisible(result))
 }
-
-
