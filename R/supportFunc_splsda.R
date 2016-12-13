11d001909c6401a85644caebbb146175740338e1
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2016-12-12 23:49:27 -0800

    updated tuned.splsda function

--- R/supportFunc_splsda.R
+++ R/supportFunc_splsda.R
@@ -36,7 +36,14 @@ tuned.spslda = function(X, Y, ncomp, keepXgrid, validation, M){
 #' @param iter Number of times to repeat cross-validation
 #' @param progressBar = show progress bar or not
 #' @export
-perf.tuned.splsda = function(object, validation = "Mfold", M = 5, iter = 5, progressBar = TRUE){
+perf.tuned.splsda=function (object, validation = "Mfold", M = 5, iter = 5, progressBar = TRUE){
+  if(is.null(Y))
+    stop("names(Y) should equal rownams(X)")
+  for(i in 1 : nlevels(Y)){
+    if(levels(Y)[i] == "ER")
+      stop("levels of Y include ER or BER")
+  }
+
   library(dplyr)
   library(tidyr)
   X = object$X
@@ -45,39 +52,53 @@ perf.tuned.splsda = function(object, validation = "Mfold", M = 5, iter = 5, prog
   keepXgrid = object$keepXgrid
   validation = object$validation
   M = object$M
+  n = nrow(X)
   if (validation == "Mfold") {
     folds <- lapply(1:iter, function(i) createFolds(Y, k = M))
     cvList <- list()
-    for(i in 1 : length(folds)){
-      cvList[[i]] <- tuned.spsldaCV(X = X, Y = Y, folds = folds[[i]], progressBar = progressBar,
-        ncomp = ncomp, keepXgrid = keepXgrid, validation = validation, M = M)
+    for (i in 1:length(folds)) {
+      cvList[[i]] <- tuned.spsldaCV(X = X, Y = Y, folds = folds[[i]],
+        progressBar = progressBar, ncomp = ncomp, keepXgrid = keepXgrid,
+        validation = validation, M = M)
     }
     cv <- cvList %>% amritr::zip_nPure()
-    errRes <- cv$errorRate %>% amritr::zip_nPure() %>% lapply(., function(j){
-      do.call(rbind, j)
-    })
+    errRes <- cv$errorRate %>% amritr::zip_nPure() %>% lapply(.,
+      function(j) {
+        do.call(rbind, j)
+      })
     errorRate <- do.call(rbind, errRes) %>% as.data.frame %>%
       mutate(Method = rep(names(errRes), each = nrow(errRes[[1]]))) %>%
-      tidyr::gather(Index, Value, -Method) %>%
-      dplyr::group_by(Index, Method) %>% dplyr::summarise(Mean = mean(Value), SD = sd(Value)) %>%
-      ungroup %>% dplyr::group_by(Method) %>%
+      tidyr::gather(Index, Value, -Method) %>% dplyr::group_by(Index,
+        Method) %>% dplyr::summarise(Mean = mean(Value),
+          SD = sd(Value)) %>% ungroup %>% dplyr::group_by(Method) %>%
       dplyr::filter(Index == "BER")
-    aucRes <- cv$auc %>% amritr::zip_nPure() %>% lapply(., function(j){
-      do.call(rbind, j)
-    })
-    auc <- do.call(rbind, aucRes) %>% as.data.frame %>%
-      mutate(Method = rep(names(errRes), each = nrow(errRes[[1]]))) %>%
-      tidyr::gather(Index, Value, -Method) %>%
-      dplyr::group_by(Index, Method) %>% dplyr::summarise(Mean = mean(Value), SD = sd(Value)) %>%
+    aucRes <- cv$auc %>% amritr::zip_nPure() %>% lapply(.,
+      function(j) {
+        do.call(rbind, j)
+      })
+    auc <- do.call(rbind, aucRes) %>% as.data.frame %>% mutate(Method = rep(names(errRes),
+      each = nrow(errRes[[1]]))) %>% tidyr::gather(Index,
+        Value, -Method) %>% dplyr::group_by(Index, Method) %>%
+      dplyr::summarise(Mean = mean(Value), SD = sd(Value)) %>%
       ungroup
-  }
-  else {
+  } else {
     folds = split(1:n, rep(1:n, length = n))
     M = n
-    cv <- tuned.spsldaCV(X = X, Y = Y, folds = foldsi, progressBar = progressBar,
-      ncomp = ncomp, keepXgrid = keepXgrid, validation = validation, M = M)
-    errorRate <- cv$errorRate
-    auc <- cv$auc
+    cv <- tuned.spsldaCV(X = X, Y = Y, folds = folds, progressBar = progressBar,
+      ncomp = ncomp, keepXgrid = keepXgrid, validation = validation,
+      M = M)
+    errorRate <- do.call(rbind, cv$errorRate) %>% as.data.frame %>%
+      dplyr::mutate(Method = rownames(.)) %>%
+      tidyr::gather(Index, Value, -Method) %>%
+      dplyr::group_by(Index, Method) %>%
+      dplyr::summarise(Mean = mean(Value), SD = sd(Value)) %>%
+      ungroup
+    auc <- do.call(rbind, cv$auc) %>% as.data.frame %>%
+      dplyr::mutate(Method = rownames(.)) %>%
+      tidyr::gather(Index, Value, -Method) %>%
+      dplyr::group_by(Index, Method) %>%
+      dplyr::summarise(Mean = mean(Value), SD = sd(Value)) %>%
+      ungroup
   }
   result = list()
   result$folds = folds
@@ -101,8 +122,9 @@ perf.tuned.splsda = function(object, validation = "Mfold", M = 5, iter = 5, prog
 #' @param validatoin "Mfold" or "loo"
 #' @param M Number of folds in the cross-validation
 #' @export
-tuned.spsldaCV = function(X = X, Y = Y, ncomp = ncomp, folds = folds, progressBar = FALSE,
-  keepXgrid = keepXgrid, validation = validation, M = M){
+tuned.spsldaCV=function (X = X, Y = Y, ncomp = ncomp, folds = folds, progressBar = FALSE,
+  keepXgrid = keepXgrid, validation = validation, M = M)
+{
   library(mixOmics)
   probs <- predictResponseList <- panel <- list()
   if (progressBar == TRUE)
@@ -117,46 +139,65 @@ tuned.spsldaCV = function(X = X, Y = Y, ncomp = ncomp, folds = folds, progressBa
     Y.train = Y[-omit]
     X.test = matrix(X[omit, ], nrow = length(omit))
     X.test = X[omit, , drop = FALSE]
-    spls.res0 = tuned.spslda(X.train, Y.train, ncomp, keepXgrid, validation, M)
-
-    testErr <- lapply(spls.res0$err$Method, function(z){
-      keepX <- rep(subset(spls.res0$err, Method == z)$keepX, ncomp)
-      spls.res = mixOmics::splsda(X.train, Y.train, ncomp, keepX, mode = "regression")
+    spls.res0 = tuned.spslda(X.train, Y.train, ncomp, keepXgrid,
+      validation, M)
+    testErr <- lapply(spls.res0$err$Method, function(z) {
+      keepX <- rep(subset(spls.res0$err, Method == z)$keepX,
+        ncomp)
+      spls.res = mixOmics::splsda(X.train, Y.train, ncomp,
+        keepX, mode = "regression")
       if (!is.null(spls.res$nzv$Position))
         X.test = X.test[, -spls.res$nzv$Position]
-      predictResponseList <- predict(object = spls.res, newdata = X.test, dist = paste(z, "dist", sep="."))$class
-      probs <- predict(object = spls.res, newdata = X.test, dist = paste(z, "dist", sep="."))$predict
-      return(list(predictResponseList = predictResponseList, probs = probs))
+      predictResponseList <- predict(object = spls.res,
+        newdata = X.test, dist = paste(z, "dist", sep = "."))$class
+      probs <- predict(object = spls.res, newdata = X.test,
+        dist = paste(z, "dist", sep = "."))$predict
+      return(list(predictResponseList = predictResponseList,
+        probs = probs))
     }) %>% zip_nPure()
     predictResponseList[[i]] <- testErr$predictResponseList
     probs[[i]] <- testErr$probs
   }
+  errorRate <- predictResponseList %>% zip_nPure() %>% lapply(.,
+    function(i) {
+      predictedLabels <- do.call(rbind, lapply(i, function(j) j[[1]]))[,
+        paste("comp", ncomp, sep = " ")]
+      trueLabels = Y[names(predictedLabels)]
+      mat <- table(trueLabels, predictedLabels)
+      mat2 <- mat
+      diag(mat2) <- 0
+      classError <- colSums(mat2)/colSums(mat)
+      er <- sum(mat2)/sum(mat)
+      ber <- mean(classError)
+      perf <- c(classError, er, ber)
+      names(perf) <- c(names(classError), "ER", "BER")
+      perf
+    })
+  if(validation == "Mfold"){
+    auc <- probs %>% zip_nPure() %>% lapply(., function(i) {
+      pb <- do.call(rbind, lapply(i, function(j) {
+        j[, , paste("dim", ncomp, sep = " ")]
+      }))
+      prob <- unlist(pb[, ncomp])
+      trueLabels = Y[rownames(pb)]
+      library(pROC)
+      library(OptimalCutpoints)
+      amritr::tperformance(weights = prob, trueLabels = trueLabels)
+    })
+  } else {
+    auc <- probs %>% zip_nPure() %>% lapply(., function(i) {
+      pb <- do.call(rbind, lapply(i, function(j) {
+        j[, , paste("dim", ncomp, sep = " ")]
+      }))
+      rownames(pb) <- rownames(X)
+      prob <- unlist(pb[, ncomp])
+      trueLabels = Y[rownames(pb)]
+      library(pROC)
+      library(OptimalCutpoints)
+      amritr::tperformance(weights = prob, trueLabels = trueLabels)
+    })
+  }
 
-  errorRate <- predictResponseList %>% zip_nPure() %>% lapply(., function(i){
-    predictedLabels <- do.call(rbind, lapply(i, function(j) j[[1]]))[, paste("comp", ncomp, sep = " ")]
-    trueLabels = Y[names(predictedLabels)]
-    mat <- table(trueLabels, predictedLabels)
-    mat2 <- mat
-    diag(mat2) <- 0
-    classError <- colSums(mat2)/colSums(mat)
-    er <- sum(mat2)/sum(mat)
-    ber <- mean(classError)
-    perf <- c(classError, er, ber)
-    names(perf) <- c(names(classError), "ER", "BER")
-    perf
-  })
-
-  auc <- probs %>% zip_nPure() %>% lapply(., function(i){
-    pb <- do.call(rbind, lapply(i, function(j){
-      j[, , paste("dim", ncomp, sep =" ")]
-    }))
-    prob <- unlist(pb[, ncomp])
-    trueLabels = Y[rownames(pb)]
-    library(pROC)
-    library(OptimalCutpoints)
-    amritr::tperformance(weights = prob, trueLabels = trueLabels)
-  })
   names(errorRate) <- names(auc) <- spls.res0$err$Method
-
   return(list(errorRate = errorRate, auc = auc))
 }
