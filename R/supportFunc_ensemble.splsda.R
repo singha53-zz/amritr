38e8a59681c5996ab0da8381d225e9edfbdd2090
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2017-01-12 23:27:24 -0800

    update supportFunc_ensemble.splsda.R

--- R/supportFunc_ensemble.splsda.R
+++ R/supportFunc_ensemble.splsda.R
@@ -85,17 +85,10 @@ ensemble.splsda = function(X.train, Y.train, keepXList, ncomp, X.test, Y.test, f
   return(list(result = result, Y.vote = Y.vote, perfTest = error, X.train = X.train, Y.train = Y.train, keepXList = keepXList, filter = filter, topranked = topranked, ncomp = ncomp))
 }
 
-
-
-
-
-
-
-
 #' Estimate test error using repeated cross-validation
 #'
 #'
-#' @param object - ensembleEnet object
+#' @param object - ensemble.splsda object
 #' @param validation = Mfold or loo
 #' @param M - # of folds
 #' @param iter - Number of iterations of cross-validation
@@ -108,57 +101,60 @@ perfEnsemble.splsda = function(object, validation = "Mfold", M = M, iter = iter,
   X <- object$X.train
   Y = object$Y.train
   n = length(Y)
-  alpha = object$alphaList
-  lambda = object$lambdaList
+  keepXList = object$keepXList
   filter = object$filter
   topranked = object$topranked
+  ncomp <- object$ncomp
+
   if (validation == "Mfold") {
     folds <- lapply(1:iter, function(i) createFolds(Y, k = M))
     require(parallel)
     cl <- parallel::makeCluster(mc <- getOption("cl.cores", threads))
-    parallel::clusterExport(cl, varlist = c("ensembleCV",
-      "ensembleEnet", "enet", "X", "Y", "alpha", "lambda",
-      "M", "folds", "progressBar", "filter", "topranked"), envir = environment())
+    parallel::clusterExport(cl, varlist = c("ensemble.splsdaCV", "sPLSDA", "X", "Y",
+      "keepXList", "ncomp", "M", "folds", "progressBar", "filter", "topranked"), envir = environment())
     cv <- parallel::parLapply(cl, folds, function(foldsi,
-      X, Y, alpha, lambda, M, progressBar, filter, topranked) {
-      ensembleCV(X = X, Y = Y, alpha = alpha, lambda = lambda,
+      X, Y, keepXList, ncomp, M, progressBar, filter, topranked) {
+      library(dplyr); library(pROC); library(OptimalCutpoints); library(amritr)
+      ensemble.splsdaCV(X = X, Y = Y, keepXList = keepXList, ncomp = ncomp,
         M = M, folds = foldsi, progressBar = progressBar, filter = filter, topranked = topranked)
-    }, X, Y, alpha, lambda, M, progressBar, filter, topranked) %>% amritr::zip_nPure()
+    }, X, Y, keepXList, ncomp, M, progressBar, filter, topranked) %>% amritr::zip_nPure()
     parallel::stopCluster(cl)
     perf <- do.call(rbind, cv$error) %>% as.data.frame %>%
-      tidyr::gather(ErrName, Err) %>%
-      dplyr::group_by(ErrName) %>%
+      mutate(Method = rownames(.)) %>%
+      tidyr::gather(ErrName, Err, -Method) %>%
+      dplyr::group_by(Method, ErrName) %>%
       dplyr::summarise(Mean = mean(Err), SD = sd(Err))
-  }
-  else {
+  } else {
     folds = split(1:n, rep(1:n, length = n))
     M = n
-    cv <- ensembleCV(X, Y, alpha, lambda, M, folds, progressBar, filter, topranked)
-    perf <- data.frame(Mean = cv$error) %>% mutate(ErrName = rownames(.))
+    cv <- ensemble.splsdaCV(X = X, Y = Y, keepXList = keepXList, ncomp = ncomp,
+      M = M, folds = foldsi, progressBar = progressBar, filter = filter, topranked = topranked)
+    perf <- cv$error %>% as.data.frame %>%
+      mutate(Method = rownames(.)) %>%
+      tidyr::gather(ErrName, Mean, -Method)
     perf$SD <- NA
   }
   result = list()
   result$perf = perf
-  method = "enetEnsemble.mthd"
-  result$meth = "enetEnsemble.mthd"
+  method = "splsdaEnsemble.mthd"
+  result$meth = "splsdaEnsemble.mthd"
   class(result) = c("perf", method)
   return(invisible(result))
 }
 
-#' Estimate cross-validation using cross-validation
+#' Estimate cross-validation error using cross-validation
 #'
 #'
 #' @param X - list of training datasets (nxpi); i number of elements
 #' @param Y - n-vector of class labels
-#' @param alpha = list of alpha values
-#' @param lambda = list of lambda values
+#' @param keepXList = list of keepX
 #' @param M - # of folds
 #' @param folds - list of length M, where each element contains the indices for samples for a given fold
 #' @param progressBar (TRUE/FALSE) - show progress bar or not
 #' @param filter - "none" or "p.value"
 #' @param topranked - # of significant features to use to build classifier
 #' @export
-ensemble.splsdaCV = function(X, Y, alpha, lambda, M, folds, progressBar, filter, topranked){
+ensemble.splsdaCV = function(X, Y, keepXList, ncomp, M, folds, progressBar, filter, topranked) {
   J <- length(X)
   assign("X.training", NULL, pos = 1)
   assign("Y.training", NULL, pos = 1)
@@ -184,30 +180,52 @@ ensemble.splsdaCV = function(X, Y, alpha, lambda, M, folds, progressBar, filter,
   for (i in 1:M) {
     if (progressBar == TRUE)
       setTxtProgressBar(pb, i/M)
-    result <- mapply(function(X.train, X.test, alpha, lambda) {
-      enet(X.train, Y.training[[i]], alpha = alpha, lambda = lambda,
-        X.test = X.test, Y.test = Y.test[[i]], family = "multinomial", filter = filter, topranked = topranked)
-    }, X.train = X.training[[i]], X.test = X.test[[i]], alpha = alpha, lambda = lambda, SIMPLIFY = FALSE)
-    predConcatList[[i]] <- do.call(cbind, lapply(result,
-      function(i) {
-        i$predictResponse
-      }))
+    result <- mapply(function(X.train, X.test, keepX) {
+
+      sPLSDA(X.train, Y.train = Y.training[[i]], keepX,
+        ncomp, X.test = X.test,
+        Y.test = Y.test[[i]], filter = filter, topranked = topranked)
+
+    }, X.train = X.training[[i]], X.test = X.test[[i]], keepX = keepXList, SIMPLIFY = FALSE)
+
+    predConcatList[[i]] <- lapply(result, function(i) {
+      do.call(cbind, i$predictResponse)
+    })
+
   }
-  predConcat <- do.call(rbind, predConcatList)
-  Y.vote <- apply(predConcat, 1, function(z) {
-    temp = table(z)
-    if (length(names(temp)[temp == max(temp)]) > 1) {
-      "zz"
-    }
-    else {
-      names(temp)[temp == max(temp)]
-    }
+
+  predConcat0 <- predConcatList %>% zip_nPure()
+  predConcat <- lapply(predConcat0, function(i) do.call(rbind, i))
+
+  predConcat <- lapply(1 : 3, function(i){
+    do.call(cbind, lapply(predConcat, function(j){
+      j[, i]
+    }))
   })
-  temp = table(pred = factor(Y.vote, levels = c(levels(Y),
-    "zz")), truth = unlist(Y)[unlist(folds)])
-  diag(temp) <- 0
-  error = c(colSums(temp)/summary(Y), sum(temp)/length(Y),
-    mean(colSums(temp)/summary(Y)))
-  names(error) <- c(names(error)[1:nlevels(Y)], "ER", "BER")
+
+  Y.vote <- lapply(predConcat, function(i) {
+    apply(i, 1, function(z) {
+      temp = table(z)
+      if (length(names(temp)[temp == max(temp)]) >
+          1) {
+        "zz"
+      }
+      else {
+        names(temp)[temp == max(temp)]
+      }
+    })
+  })
+  error = do.call(rbind, lapply(Y.vote, function(i) {
+    temp <- table(pred = factor(i, levels = c(levels(Y),
+      "zz")), truth = unlist(Y))
+    diag(temp) <- 0
+    error = c(colSums(temp)/summary(Y), sum(temp)/length(Y),
+      mean(colSums(temp)/summary(Y)))
+    names(error) <- c(names(error)[1:nlevels(Y)],
+      "ER", "BER")
+    error
+  }))
+  rownames(error) <- colnames(predConcat0[[1]][[1]])
+
   return(list(error = error))
 }
