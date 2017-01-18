7f240e07058e001fe57af1f493d49d2fe8d74427
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2017-01-18 00:42:39 -0800

    update compVar()

--- R/pcaFunctions.R
+++ R/pcaFunctions.R
@@ -5,43 +5,47 @@
 #' @param trubeLabels are the true labels associated with the test data
 #' @param direction = "auto", ">", "<"
 #' @export
-compVar = function(demo, eset, variables, ncomp = 10){
+compVar = function (demo, eset, variables, ncomp = 10) {
   library(RColorBrewer)
   library(dplyr)
   library(tidyr)
   library(ggplot2)
-
   pcaX <- prcomp(eset, scale. = TRUE, center = TRUE)
+  pval <- do.call(rbind, lapply(variables, function(i) {
+    apply(pcaX$x, 2, function(j) {
+      predictor <- demo[, i]
 
-  pval <- do.call(rbind, lapply(variables, function(i){
-    apply(pcaX$x, 2, function(j){
-      coef(summary(lm(as.numeric(j) ~ demo[, i])))[2, "Pr(>|t|)"]
+      if(class(predictor) == "factor"){
+        if(nlevels(predictor) == 2){
+          coef(summary(lm(as.numeric(j) ~ predictor)))[2, "Pr(>|t|)"]
+        } else {
+          anova(lm(as.numeric(j) ~ predictor))["predictor", "Pr(>F)"]
+        }
+      } else {
+        coef(summary(lm(as.numeric(j) ~ predictor)))[2, "Pr(>|t|)"]
+      }
     })
   }))
   rownames(pval) <- variables
-  colnames(pval) <- paste(colnames(pval), paste0(round(100*(pcaX$sdev/sum(pcaX$sdev)), 1), "%"), sep = "-")
-
+  colnames(pval) <- paste(colnames(pval), paste0(round(100 *
+      (pcaX$sdev/sum(pcaX$sdev)), 1), "%"), sep = "-")
   pval <- pval[, 1:ncomp]
   pvalheatmap <- pval
   pvalheatmap[pvalheatmap < 0.01] <- 0.01
   pvalheatmap[pvalheatmap > 0.1] <- 1
   pvalheatmap[pvalheatmap > 0.01 & pvalheatmap < 0.05] <- 0.05
-  pvalheatmap[pvalheatmap > 0.05 & pvalheatmap < 0.1] <- 0.10
+  pvalheatmap[pvalheatmap > 0.05 & pvalheatmap < 0.1] <- 0.1
   pvalheatmap[pvalheatmap == "0.01"] <- "p < 0.01"
   pvalheatmap[pvalheatmap == "0.05"] <- "p < 0.05"
   pvalheatmap[pvalheatmap == "0.1"] <- "p < 0.10"
   pvalheatmap[pvalheatmap == "1"] <- "p > 0.10"
-
-  ## plot
   p <- pvalheatmap %>% as.data.frame %>% mutate(Variable = rownames(.)) %>%
-    gather(Threshold, Value, -Variable) %>%
-    mutate(Threshold = factor(Threshold, levels = unique(Threshold))) %>%
-    mutate(Variable = factor(as.character(Variable), levels = variables)) %>%
-    ggplot(aes(Threshold, Variable)) + geom_tile(aes(fill = Value),
-      colour = "white") + scale_fill_manual(values=rev(brewer.pal(n = 8, name = "Blues")[c(2,4,6,8)])) +
-    customTheme(sizeStripFont = 10, xAngle = 40, hjust = 1, vjust = 1,
-      xSize = 10, ySize = 10, xAxisSize = 10, yAxisSize = 10) +
-    xlab("") + ylab("")
-
-  return(list(pval=pval, pvalheatmap=pvalheatmap, p=p))
+    gather(Threshold, Value, -Variable) %>% mutate(Threshold = factor(Threshold,
+      levels = unique(Threshold))) %>% mutate(Variable = factor(as.character(Variable),
+        levels = variables)) %>% ggplot(aes(Threshold, Variable)) +
+    geom_tile(aes(fill = Value), colour = "white") + scale_fill_manual(values = rev(brewer.pal(n = 8,
+      name = "Blues")[c(2, 4, 6, 8)])) + customTheme(sizeStripFont = 10,
+        xAngle = 40, hjust = 1, vjust = 1, xSize = 10, ySize = 10,
+        xAxisSize = 10, yAxisSize = 10) + xlab("") + ylab("")
+  return(list(pval = pval, pvalheatmap = pvalheatmap, p = p))
 }
