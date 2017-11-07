6403138fc1d3ab1c31346342acec5c739c573c06
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2017-11-07 09:26:51 -0500

    add hypertest to GSA functions

--- R/geneset_analysis.R
+++ R/geneset_analysis.R
@@ -49,3 +49,28 @@ cat(paste0("% of unmapped identifiers in the background set = ",
 rbind(unfiltered, filtered)
 }
 
+
+#' @title hypergeometric Test
+#' @description
+#' \code{hypertest} performs hypergeometric tests across a list of gene sets
+#' @param genelist vector of gene symbols
+#' @param genesets list of gene sets
+#' @details
+#' This function uses the hypergeometric function to test for over-representation of genesets in a user-specified list of genes.
+#' @return The output is a dataframe with 3 columns (gene sets (genesets), p-value (pval), and bhfdr (BH-FDR)
+#' @export
+hypertest = function(genelist, genesets){
+  data.frame(pval = sapply(genesets, function(i){
+    uni <- unlist(genesets)
+    input <- genelist
+    n_input = length(input)
+    n_geneset = length(i)
+    intersect = length(intersect(input, i))
+    stats::phyper(intersect - 1, n_input,
+      length(uni) - n_input, n_geneset, lower.tail = F)
+  })) %>%
+    mutate(genesets = rownames(.),
+      bhfdr = p.adjust(pval, "BH")) %>%
+    arrange(pval) %>%
+    dplyr::select(genesets, pval, bhfdr)
+}
