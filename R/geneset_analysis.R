85280f90a366aa6bf0ef115cb66e33c2e9710164
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2017-11-08 12:52:09 -0500

    update functions and add new pathway and perturbation datasets

--- R/geneset_analysis.R
+++ R/geneset_analysis.R
@@ -22,31 +22,56 @@
 #'
 #' result %>% group_by(Type) %>% slice(1:5)
 #' @export
-gene_set_analysis = function(genelist,
-                             genesets,
-                             universe){
-# unfiltered
-unfiltered <- hypertest(genelist, genesets)
-unfiltered$Type <- "unfiltered"
+gene_set_analysis = function(genes, genesets, study_universe, alpha, topranked = 5, min.set.size = 1){
+  ## unfiltered results
+  genesets$universe <- genesets$Genes %>% unlist %>%
+    unique %>% length
+  unfiltered <- genesets %>%
+    dplyr::rowwise(.) %>%
+    dplyr::mutate(n_geneset = length(Genes)) %>%
+    dplyr::filter(n_geneset > min.set.size) %>%
+    dplyr::mutate(n_input = length(genes),
+      overlap = length(intersect(genes, Genes)),
+      p_value = phyper(overlap - 1, n_geneset,
+        universe - n_geneset, n_input, lower.tail = F)) %>%
+    ungroup() %>%
+    dplyr::group_by(PathwayType) %>%
+    dplyr::mutate(bhfdr = p.adjust(p_value, method = "BH")) %>%
+    dplyr::arrange(PathwayType, p_value)
+  subset.unfiltered <- summarise(unfiltered, sig=sum(bhfdr < alpha)) %>%
+    filter(sig != 0) %>% dplyr::select(PathwayType)
+  unfiltered <- rbind(filter(unfiltered, PathwayType %in% subset.unfiltered$PathwayType) %>% filter(bhfdr < alpha),
+    filter(unfiltered, !(PathwayType %in% subset.unfiltered$PathwayType)) %>% slice(1:topranked))
 
-## filtered
-filtered_genesets <- lapply(genesets, function(i) intersect(i, universe))
-filtered <- hypertest(genelist, filtered_genesets)
-filtered$Type <- "filtered"
+  # filter genes using study_universe
+  genesets$universe <- genesets$Genes %>% unlist %>%
+    intersect(., study_universe) %>% unique %>% length
+  filtered <- genesets %>%
+    dplyr::rowwise(.) %>%
+    dplyr::mutate(Genes = list(intersect(Genes, study_universe)),
+      n_geneset = length(Genes)) %>%
+    dplyr::filter(n_geneset > min.set.size) %>%
+    dplyr::mutate(n_input = length(genes),
+      overlap = length(intersect(genes, Genes)),
+      p_value = phyper(overlap - 1, n_geneset,
+        universe - n_geneset, n_input, lower.tail = F)) %>%
+    ungroup() %>%
+    dplyr::group_by(PathwayType) %>%
+    dplyr::mutate(bhfdr = p.adjust(p_value, method = "BH")) %>%
+    arrange(PathwayType, p_value)
+  subset.filtered <- summarise(filtered, sig=sum(bhfdr < alpha)) %>%
+    filter(sig != 0) %>% dplyr::select(PathwayType)
+  filtered <- rbind(filter(filtered, PathwayType %in% subset.filtered$PathwayType) %>% filter(bhfdr < alpha),
+    filter(unfiltered, !(PathwayType %in% subset.filtered$PathwayType)) %>% slice(1:topranked))
 
-## print percentage of mapping and unmapped identifiers in the unfiltered genelist
-cat(paste0("% of mapped identifiers in unfiltered genesets = ",
-  100*round(length(intersect(genelist, unlist(genesets)))/length(genelist), 2), "%"), fill = TRUE)
-cat(paste0("% of unmapped identifiers in unfiltered genesets = ",
-  100*round(length(setdiff(genelist, unlist(genesets)))/length(genelist), 2), "%"), fill = TRUE)
 
-## print percentage of overlapping and non-overlapping identifiers in the background set
-cat(paste0("% of mapped identifiers in the background set = ",
-  100*round(length(intersect(universe, unlist(genesets)))/length(universe), 2), "%"), fill = TRUE)
-cat(paste0("% of unmapped identifiers in the background set = ",
-  100*round(length(setdiff(universe, unlist(genesets)))/length(universe), 2), "%"), fill = TRUE)
+  ## Proportion of mapped background set
+  prop.mapped.backgroundSet <- unfiltered %>% summarise(int = round(100*length(intersect(unique(unlist(Genes)), study_universe))/length(study_universe), 1))
 
-rbind(unfiltered, filtered)
+  prop <- cbind(mapped.genes.unfiltered, mapped.genes.filtered[,-1], prop.mapped.backgroundSet[,-1])
+  colnames(prop) <- c("PathwayType", "mapped.genes.unfiltered", "mapped.genes.filtered", "prop.mapped.backgroundSet")
+
+  return(list(unfiltered=unfiltered, filtered=filtered, prop=prop))
 }
 
 
@@ -59,18 +84,27 @@ rbind(unfiltered, filtered)
 #' This function uses the hypergeometric function to test for over-representation of genesets in a user-specified list of genes.
 #' @return The output is a dataframe with 3 columns (gene sets (genesets), p-value (pval), and bhfdr (BH-FDR)
 #' @export
-hypertest = function(genelist, genesets){
-  data.frame(pval = sapply(genesets, function(i){
+hypertest=function (genelist, genesets, cpus) {
+  require(parallel)
+  cl <- parallel::makeCluster(mc <- getOption("cl.cores", cpus))
+  parallel::clusterExport(cl, varlist = c("genesets", "genelist"), envir = environment())
+  pval <- parallel::parLapply(cl, genesets, function(genesetsi, genelist) {
     uni <- unlist(genesets)
     input <- genelist
     n_input = length(input)
-    n_geneset = length(i)
-    intersect = length(intersect(input, i))
-    stats::phyper(intersect - 1, n_input,
-      length(uni) - n_input, n_geneset, lower.tail = F)
-  })) %>%
-    mutate(genesets = rownames(.),
+    n_geneset = length(genesetsi)
+    intersect = length(intersect(input, genesetsi))
+    data.frame(pval=stats::phyper(intersect - 1, n_input, length(uni) - n_input,
+      n_geneset, lower.tail = F),
+      Genes=paste(intersect(input, genesetsi), collapse=";"),
+      Overlap=paste0(intersect, "/", n_geneset))
+  }, genelist)
+  parallel::stopCluster(cl)
+  pval %>% do.call(rbind, .) %>%
+    as.data.frame %>% mutate(genesets = rownames(.),
       bhfdr = p.adjust(pval, "BH")) %>%
-    arrange(pval) %>%
-    dplyr::select(genesets, pval, bhfdr)
+    arrange(pval) %>% dplyr::select(genesets,
+      Overlap,
+      pval, bhfdr, Genes)
 }
+
